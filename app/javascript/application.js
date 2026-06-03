78da5a529482f3f04dfd659d8c047b0fdd6a2357
epcocryo <epcsgokb@gmail.com>
2026-06-04 08:08:34 +0900

    add: ヘッダー作成・ハンバーガーメニュー実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+// ハンバーガーメニューの表示/非表示を切り替える
+document.addEventListener('turbo:load', function() {
+  const button = document.getElementById('hamburger-button');
+  const menu = document.getElementById('hamburger-menu');
+  
+  if (button && menu) {
+    button.addEventListener('click', function() {
+      if (menu.style.display === 'none') {
+        menu.style.display = 'block';
+      } else {
+        menu.style.display = 'none';
+      }
+    });
+  }
+});
