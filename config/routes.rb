d07d15362159e1c3a5f4e83680ec5e08db785044
epcocryo <epcsgokb@gmail.com>
2026-06-04 23:57:00 +0900

    プライバシーポリシーページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   get "terms", to: "static_pages#terms"
+  get "privacy_policy", to: "static_pages#privacy_policy"
   # Defines the root path route ("/")
   # root "posts#index"
 end
