c08479382bd1c02a15368f3fabeb7259ca371fb5
epcocryo <epcsgokb@gmail.com>
2026-07-08 05:28:42 +0900

    フラッシュメッセージ追加

---
+++ app/javascript/flash.js
@@ -0,0 +1,19 @@
+const FLASH_DISPLAY_DURATION = 3000;
+const FLASH_FADE_DURATION = 800;
+
+function setupFlashMessages() {
+  const flashMessages = document.querySelectorAll(".flash-message");
+
+  flashMessages.forEach((flash) => {
+    setTimeout(() => {
+      flash.classList.add("fade-out");
+
+      setTimeout(() => {
+        flash.remove();
+      }, FLASH_FADE_DURATION);
+    }, FLASH_DISPLAY_DURATION);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", setupFlashMessages);
+document.addEventListener("turbo:load", setupFlashMessages);
