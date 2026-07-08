318bae98e848136f71d4d9adc41a2561608ddda1
epcocryo <epcsgokb@gmail.com>
2026-07-09 08:33:30 +0900

    fix: Rubocopのエラーを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   # ✨ profile アクションが実行される前に、Deviseのログインチェックを動かす！
-  before_action :authenticate_user!, only: [:profile]
+  before_action :authenticate_user!, only: [ :profile ]
 
   def profile
     # ここにプロフィール画面用の処理を書きます
