d352b521083e448afe68faf5383de8f9d2c88fc1
epcocryo <epcsgokb@gmail.com>
2026-08-17 04:52:20 +0900

    fix: CI環境でのSystem Spec設定を修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,20 +5,20 @@ if ENV['CI']
 
   Capybara.register_driver :remote_chrome do |app|
     options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('no-sandbox')
-    options.add_argument('headless')
-    options.add_argument('disable-gpu')
-    options.add_argument('disable-dev-shm-usage')
-    options.add_argument('window-size=1680,1050')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1680,1050')
 
     Capybara::Selenium::Driver.new(
       app,
       browser: :chrome,
-      capabilities: options
+      options: options
     )
   end
 else
-
+  
   Capybara.register_driver :remote_chrome do |app|
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('no-sandbox')
