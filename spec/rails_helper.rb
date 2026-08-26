e1a323dda4d60635036682b25aeac7f14c7eb533
epcocryo <epcsgokb@gmail.com>
2026-08-26 19:51:39 +0900

    test: Capybaraのデフォルト待機時間を10秒に延長

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,7 +48,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  Capybara.default_max_wait_time = 10
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
