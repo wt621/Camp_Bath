6e6c453bf9060aed96e03477006bb63a8775feb8
epcocryo <epcsgokb@gmail.com>
2026-09-15 03:23:05 +0900

    lintエラーの修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   devise_for :users, skip: [ :passwords ], controllers: {
     registrations: "users/registrations"
   }
-  get '/test/transaction_check', to: 'test_transactions#check'
-  
+  get "/test/transaction_check", to: "test_transactions#check"
+
   root "maps#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
