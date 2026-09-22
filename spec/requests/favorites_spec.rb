73755bde35a69f057ba351474a3146275fbf6cd9
epcocryo <epcsgokb@gmail.com>
2026-09-22 19:17:52 +0900

    test:お気に入り機能のテストを追加

--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe "Favorites", type: :request do
         expect(favorite.user).to eq(user)
         expect(favorite.campsite_place_id).to eq("campsite-place-id")
         expect(favorite.onsen_place_id).to eq("onsen-place-id")
+        expect(response.parsed_body).to eq({ "saved" => true })
       end
 
       it "同じセットが保存済みなら削除されること" do
@@ -39,12 +40,106 @@ RSpec.describe "Favorites", type: :request do
             onsen_place_id: "onsen-place-id"
           }
         }.to change(Favorite, :count).by(-1)
+        expect(response.parsed_body).to eq({ "saved" => false })
       end
     end
 
     context "ログインしていない場合" do
       it "ログインページへリダイレクトされること" do
         post favorites_path, params: {
+          campsite_place_id: "campsite-place-id",
+          onsen_place_id: "onsen-place-id"
+        }
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe "GET /favorites" do
+    context "ログインしている場合" do
+      before do
+        sign_in user
+      end
+
+      it "お気に入りページを表示できること" do
+        get favorites_path
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "ログイン中のユーザーのお気に入りだけを取得すること" do
+        favorite = create(
+          :favorite,
+          user: user,
+          campsite_place_id: "user-campsite-place-id",
+          onsen_place_id: "user-onsen-place-id"
+        )
+
+        other_user = create(:user)
+
+        other_favorite = create(
+          :favorite,
+          user: other_user,
+          campsite_place_id: "other-campsite-place-id",
+          onsen_place_id: "other-onsen-place-id"
+        )
+
+        get favorites_path
+
+        expect(response.body).to include(favorite.campsite_place_id)
+        expect(response.body).to include(favorite.onsen_place_id)
+        expect(response.body).not_to include(other_favorite.campsite_place_id)
+        expect(response.body).not_to include(other_favorite.onsen_place_id)
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "ログインページへリダイレクトされること" do
+        get favorites_path
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe "GET /favorites/check" do
+    context "ログインしている場合" do
+      before do
+        sign_in user
+      end
+
+      it "お気に入り登録済みの場合はsavedがtrueになること" do
+        create(
+          :favorite,
+          user: user,
+          campsite_place_id: "campsite-place-id",
+          onsen_place_id: "onsen-place-id"
+        )
+
+        get favorites_check_path, params: {
+          campsite_place_id: "campsite-place-id",
+          onsen_place_id: "onsen-place-id"
+        }
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body).to eq({ "saved" => true })
+      end
+
+      it "お気に入り登録されていない場合はsavedがfalseになること" do
+        get favorites_check_path, params: {
+          campsite_place_id: "campsite-place-id",
+          onsen_place_id: "onsen-place-id"
+        }
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body).to eq({ "saved" => false })
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "ログインページへリダイレクトされること" do
+        get favorites_check_path, params: {
           campsite_place_id: "campsite-place-id",
           onsen_place_id: "onsen-place-id"
         }
--- spec/system/favorites_spec.rb
+++ spec/system/favorites_spec.rb
@@ -22,6 +22,67 @@ RSpec.describe 'お気に入りページ', type: :system do
 
       expect(page).to have_content('キャンプ場・温泉施設')
     end
+
+    it 'お気に入り登録済みのセットが表示されること' do
+      create(
+        :favorite,
+        user: user,
+        campsite_place_id: 'campsite-place-id',
+        onsen_place_id: 'onsen-place-id'
+      )
+
+      visit favorites_path
+
+      expect(page).to have_css(
+        '[data-favorite-item][data-campsite-place-id="campsite-place-id"]'
+      )
+      expect(page).to have_css(
+        '[data-favorite-item][data-onsen-place-id="onsen-place-id"]'
+      )
+      expect(page).to have_button('お気に入りから削除')
+    end
+
+    it 'お気に入りセットから検索ページへ遷移できること' do
+      create(
+        :favorite,
+        user: user,
+        campsite_place_id: 'campsite-place-id',
+        onsen_place_id: 'onsen-place-id'
+      )
+
+      visit favorites_path
+
+      expect(page).to have_link(
+        href: search_path(
+          campsite_place_id: 'campsite-place-id',
+          onsen_place_id: 'onsen-place-id'
+        )
+      )
+    end
+
+    it 'お気に入りから削除するとセットが表示されなくなること' do
+      create(
+        :favorite,
+        user: user,
+        campsite_place_id: 'campsite-place-id',
+        onsen_place_id: 'onsen-place-id'
+      )
+
+      visit favorites_path
+
+      expect(page).to have_css(
+        '[data-favorite-item][data-campsite-place-id="campsite-place-id"]'
+      )
+
+      expect do
+        find(
+          '[data-favorite-item][data-campsite-place-id="campsite-place-id"]'
+        ).find_button('お気に入りから削除').click
+        expect(page).to have_no_css(
+          '[data-favorite-item][data-campsite-place-id="campsite-place-id"]'
+        )
+      end.to change(Favorite, :count).by(-1)
+    end
   end
 
   describe 'ログインしていない場合' do
